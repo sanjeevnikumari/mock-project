afe28fef725a1c9f17478c1ec364460f7eb89808
Sanjeevni Kumari <sanjeevnikumari12@gmail.com>
2025-07-09 11:07:57 -0700

    added mongodb

--- server/index.js
+++ server/index.js
@@ -53,10 +53,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect("mongodb://127.0.0.1:27017/employee", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB Connected"))
 .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
